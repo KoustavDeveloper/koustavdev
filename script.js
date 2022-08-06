6210c4e2b971564e4a085dd2c1be755339a9ff98
Koustav Singh <97940859+KoustavDeveloper@users.noreply.github.com>
2022-08-06 10:06:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Gamer", "Web Designer", "Hardware Expert", "PC Assembler"],
+        strings: ["Student","Developer", "Gamer", "Web Designer", "Hardware Expert", "PC Assembler"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Gamer", "Web Designer", "Hardware Expert", "PC Assembler"],
+        strings: [["Student","Developer", "Gamer", "Web Designer", "Hardware Expert", "PC Assembler"],
         typeSpeed: 100,
         backSpeed: 40,
         loop: true
